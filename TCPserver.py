8e20cd9a7948f0e9ef35240495832deae03fae41
skagelysgaard <sly013@uib.no>
2021-03-23 11:57:26 +0100

    addet features i TCPserver

--- TCPserver.py
+++ TCPserver.py
@@ -1,6 +1,15 @@
 
 import threading
+<<<<<<< HEAD
 from socket import create_server
-#is this tcp? yes/n
+#is this tcp? yes
 server = create_server(("localhost", 5550))
+=======
+import socket
+>>>>>>> addet features i TCPserver
 
+host = "localhost" 
+port = 5550
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.listen()
