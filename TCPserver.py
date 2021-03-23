bf59d0224c7c6b5103a4f34b44963644d58df6e9
skagelysgaard <sly013@uib.no>
2021-03-23 12:03:19 +0100

    ny tcp server

--- TCPserver.py
+++ TCPserver.py
@@ -1,6 +1,11 @@
 
 import threading
-from socket import create_server
+import socket
 #is this tcp? yes/n
 server = create_server(("localhost", 5550))
 
+host = "localhost" # localhost
+port = 5550
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.listen()
