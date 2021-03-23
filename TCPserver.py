ef1d2da9d00a472154319f5b78536e96b37a0c77
skagelysgaard <sly013@uib.no>
2021-03-23 12:01:54 +0100

    errorfix

--- TCPserver.py
+++ TCPserver.py
@@ -1,15 +1,6 @@
 
 import threading
-<<<<<<< HEAD
 from socket import create_server
-#is this tcp? yes
+#is this tcp? yes/n
 server = create_server(("localhost", 5550))
-=======
-import socket
->>>>>>> addet features i TCPserver
 
-host = "localhost" 
-port = 5550
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.listen()
