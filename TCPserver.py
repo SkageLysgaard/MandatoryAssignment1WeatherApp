9de7b656ad5be7100a77163b52f680d4720ae892
skagelysgaard <sly013@uib.no>
2021-03-23 12:05:52 +0100

    tcp server fix

--- TCPserver.py
+++ TCPserver.py
@@ -1,11 +1,12 @@
 
 import threading
 import socket
-#is this tcp? yes/n
-server = create_server(("localhost", 5550))
+
 
 host = "localhost" # localhost
 port = 5550
 
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
 server.listen()
