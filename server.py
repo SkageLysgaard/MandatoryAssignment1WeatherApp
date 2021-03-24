cccca45e7bd7fe3fb81e4e021244ba83130c2e00
SkageLysgaard <31646924+SkageLysgaard@users.noreply.github.com>
2021-03-24 19:48:21 +0100

    Update server.py
    implementert savefile.py inn i server.py

--- server.py
+++ server.py
@@ -1,13 +1,47 @@
 from socket import create_server
+from time import sleep
+from station import StationSimulator
 
 server = create_server(("localhost", 5550))
 print("host")
 print("Waiting for ant incomming connections...")
 conn, addr = server.accept()
-print(addr, "Has connected to the server")
+location = conn.recv(1024).decode()
+print(addr, "Has connected to the server", location)
+ 
+ 
 
-filename = input(str("please enter the filename of the file: "))
+
+#if filename == bergen:
+if __name__ == "__main__":
+    bergen_station = StationSimulator(simulation_interval=1)
+    bergen_station.turn_on()
+
+    temperature = []
+    precipitation = []
+
+    for _ in range(72):
+        # Sleep for 1 second to wait for new weather data
+        # to be simulated
+        sleep(1)
+        # Read new weather data and append it to the
+        # corresponding list
+        temperature.append(bergen_station.temperature)
+        precipitation.append(bergen_station.rain)
+
+    bergen_station.shut_down()
+    
+    f = open("bergen.py", "w")
+    f.write("temperature " + str(temperature) + "\n")
+    f.write("precipitation " + str(precipitation))
+    f.close()
+else:
+    print("Sorry, i didn't get that")
+
+    
+
+filename = location
 file = open(filename , "rb")
 file_data = file.read(1024)
 conn.send(file_data)
-print("Data has been sent successfully!")
+print("Data has been sendt successfully!")
