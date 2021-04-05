343b36f02324b55208605f64e5ef83ec8004bcd9
AsmundGroven <81160202+AsmundGroven@users.noreply.github.com>
2021-04-05 19:58:30 +0200

    Update mongodb.py
    La inn passord

--- mongodb.py
+++ mongodb.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient 
 
-cluster = MongoClient("mongodb://adminGroven:<password>@weatherstoragedb-shard-00-00.8q1zq.mongodb.net:27017,weatherstoragedb-shard-00-01.8q1zq.mongodb.net:27017,weatherstoragedb-shard-00-02.8q1zq.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ubvlc6-shard-0&authSource=admin&retryWrites=true&w=majority")
+cluster = MongoClient("mongodb://adminGroven:<12345678!>@weatherstoragedb-shard-00-00.8q1zq.mongodb.net:27017,weatherstoragedb-shard-00-01.8q1zq.mongodb.net:27017,weatherstoragedb-shard-00-02.8q1zq.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ubvlc6-shard-0&authSource=admin&retryWrites=true&w=majority")
 database = cluster["cluster"]
 collection = database["collection"]
 
