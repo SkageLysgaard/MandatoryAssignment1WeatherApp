8840834df9ec759f50c68f537ed266a6f630c88f
sondresylte <sondre.sylte@student.uib.no>
2021-04-06 13:43:22 +0200

    Rydder opp i filer

---
+++ FørsteUtkast/TCP_send_storage.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from socket import socket, AF_INET, SOCK_STREAM
+
+app = Flask(__name__)
+
+@app.route("location")
+def recieveLocation():
+    pass
+@app.route("month")
+def recieveMonth():
+    pass
+@app.route("temperature")
+def recieveTemperature():
+    pass
+@app.route("rain")
+def recieveRain():
+    
+
+
