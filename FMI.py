0823b2982fb96d4b8813c662262bbf6f18f65eae
Åsmund Groven <asmundgroven@pc101-196.eduroam.uib.no>
2021-04-06 13:41:26 +0200

    fikset på STORAGE

--- FMI.py
+++ FMI.py
@@ -1,3 +1,6 @@
 from socket import socket, create_connection
+from flask import Flask
 
 sock = create_connection("locahost",5556)
+
+
--- STORAGE.py
+++ STORAGE.py
@@ -1,22 +1,24 @@
 from socket import socket, AF_INET,SOCK_STREAM, SOCK_DGRAM
+from threading import Thread
 
 socketTCP = socket(AF_INET,SOCK_STREAM)
 socketUDP = socket(AF_INET,SOCK_DGRAM)
 
 socketTCP.bind(("localhost", 5555))
 socketUDP.bind(("localhost", 5556))
-socketTCP.listen(5)
-socketUDP.listen(5)
+socketTCP.listen()
+
 
 print("[LISTENING] The server is listening...")
 
 def start():
     while True:
         conn, addr= socketTCP.accept()
+        _ , UDP_adress = socketUDP.recvfrom(2048)
         data = conn.recv(1024).decode()
-        ct = client_thread(conn)
-        conn.send(new_sentence.encode())
-        print(f'Received message from {addr}, message: {data}')
+        ct = Thread(conn)
+        conn.send(data.encode())
+        print(f'Received message from {addr, UDP_adress}, message: {data}')
         ct.run()
         conn.close()
 
