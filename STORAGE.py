3b7f2370a8a6e59f7c35d75c4dc38bb45b245176
Åsmund Groven <asmundgroven@pc29-196.eduroam.uib.no>
2021-04-07 11:44:46 +0200

    changes

--- STORAGE.py
+++ STORAGE.py
@@ -23,22 +23,26 @@ def start():
     print("[LISTENING] The server is listening on ...")
     while True:
         conn, TCP_addr = socketTCP.accept()
-        #weatherData , UDP_adress = socketUDP.recvfrom(2048)
-        data = conn.recv(1024).decode()
         #ct = Thread(conn)
-        encoded = data.encode()
-        #Dersom inputen fra CLI er x:
-        print(f'{data}')
-        if(encoded == "1"):#Dersom meldingen fra FMI er 1 aka "rain":
-                with open("DATA.txt") as data:
-                    reader = csv.reader(data, delimiter = "\t")
-                    for row in reader: 
-                        conn.send(row)
-        #Dersom 
-        print(f'Received message from {TCP_addr}, message: {data}')
-        #print(f'Received message from {UDP_adress}, message: {data}') Denne funker ikke, av en eller annen grunn
-        #ct.run()
-        conn.close()
+        weatherData , UDP_adress = socketUDP.recvfrom(2048)
+        #ct1 = Thread(weatherData)
+        data = conn.recv(1024).decode()
+        weatherData.sendto(data, UDP_adress)
+        if data:
+            encoded = data.encode()
+            #Dersom inputen fra CLI er x:
+            print(data)
+            if(data == "1"):#Dersom meldingen fra FMI er 1 aka "rain":
+                    with open("DATA.txt") as data:
+                        reader = csv.reader(data, delimiter = "\t")
+                        for row in reader: 
+                            pass
+            #Dersom 
+            print(f'Received message from {TCP_addr}, message:')
+            print(encoded)
+            #print(f'Received message from {UDP_adress}, message: {data}') #Denne funker ikke, av en eller annen grunn
+            #ct.run()
+            conn.close()
 print("[STARTING] server is starting")
 start()
 
