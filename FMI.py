2b4c7752397d7e22cd473b799797b0074bb30062
Åsmund Groven <asmundgroven@pc40-196.eduroam.uib.no>
2021-04-08 11:12:52 +0200

    FMI

--- FMI.py
+++ FMI.py
@@ -8,21 +8,27 @@ print("Weather data:")
 print(f'WRITE FOLLOWING KEYWORD TO RECEIVE CORRESPONDING DATA: \n') 
 print(f'BERGEN: Receive all data from BERGEN \n')
 print(f'OSLO: Receive all data from OSLO \n')
-print(f'TROMSO: Receive all data from TROMSO \n')
+print(f'TROMSO: Receive all data from TROMSO \n \n')
+print(f'DISCONNECT : Disconnects from the server')
+
+DISCONNECT_MESSAGE = 'DISCONNECT'
 
 
 while True:
-    numbers = str(input("Enter CITY: "))
-    #sends the location to the server
-    msg = numbers.encode("utf-8")
-    hello =  client.send(msg)
-    msg_receievd, _ = client.recvfrom(2048)
-    msg_2, _ = client.recvfrom(2048)
-    msg_3, _ = client.recvfrom(2048)
+    msg = str(input("Enter CITY: "))
+    if (msg != DISCONNECT_MESSAGE):
+        #sends the location to the server
+        msg = msg.encode("utf-8")
+        hello =  client.send(msg)
+        msg_receievd, _ = client.recvfrom(2048)
+        msg_2, _ = client.recvfrom(2048)
+        msg_3, _ = client.recvfrom(2048)
 
-    print('RECEIVING DATA: ')
-    print(msg_receievd.decode())
-    print(msg_2.decode())
+        print('RECEIVING DATA: ')
+        print(msg_receievd.decode())
+        print(msg_2.decode())
+    elif (msg == DISCONNECT_MESSAGE): 
+        break
 
 
 
